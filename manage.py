a8703ac0643920e7d5c07811167b5f6a626c65df
Benjamin Dunsford <benjamindunsford943@gmail.com>
2023-05-27 17:40:02 -0500

    still need dotenv

--- manage.py
+++ manage.py
@@ -2,6 +2,12 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+import discord
+
+
+from dotenv import load_dotenv
+project_folder = os.path.expanduser('~/web_project')  # adjust as appropriate
+load_dotenv(os.path.join(project_folder, '.env'))
 
 
 def main():
@@ -20,3 +26,24 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+TOKEN = os.getenv('TOKEN')
+GUILD = os.getenv('GUILD')
+
+
+intents = discord.Intents.default()
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    for guild in client.guilds:
+        if guild.name == GUILD:
+            break
+
+    print(
+        f'{client.user} is connected to the following guild:\n'
+        f'{guild.name}(id: {guild.id})'
+    )
+
+client.run(TOKEN)
+
